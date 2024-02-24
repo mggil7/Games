eae98dad76803f2b3ab2d3aa064eed843472ff61
MIGUEL GIL <miguel.gustavo.gil@gmail.com>
2024-02-24 15:18:49 -0300

    on Mouse live coords on CirclesOnScreen

--- CirclesOnScreen/src/components/CirclesOnScreen.jsx
+++ CirclesOnScreen/src/components/CirclesOnScreen.jsx
@@ -1,57 +1,70 @@
-import './CirclesOnScreen.css'
-import React, {useState} from 'react'
+import "./CirclesOnScreen.css";
+import React, { useState, useEffect } from "react";
 
 const CirclesOnScreen = () => {
+  const [pontos, setPontos] = useState([]);
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
 
-    const [ pontos, setPontos ] = useState([]);
+  const handleLimpar = () => {
+    setPontos([""]);
+  };
 
-    const handleLimpar = () => {
-        setPontos(['']);
+  const addDots = (e) => {
+    const newDots = {
+      clientX: e.clientX,
+      clientY: e.clientY,
+    };
 
-    }
+    setPontos((prev) => [...prev, newDots]);
+  };
 
-    const addDots = (e) => {
-        const newDots = {
-            clientX : e.clientX,
-            clientY: e.clientY
-        }
+  useEffect(() => {
+    const handleWindowMouseMove = (event) => {
+      setCoords({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+    window.addEventListener("mousemove", handleWindowMouseMove);
 
-        setPontos((prev)=>[...prev, newDots])
-
-    }
-
+    return () => {
+      window.removeEventListener("mousemove", handleWindowMouseMove);
+    };
+  }, []);
 
   return (
+    <>
+      <div id="topGame">
+      <p>Posição do mouse:
+        <b>
+            {coords.x},{coords.y}
+          </b>
+        </p>
+        <button onClick={handleLimpar}>LIMPA</button>
 
-<>
-    <div id='topGame'>
-
-     <button onClick={handleLimpar}>LIMPA</button>
-      
         
-    </div>
-   
-    <div id="pageGame" onClick={addDots}>
+      </div>
 
-        {pontos.map((ponto,index) => {
-            return (
-                <div key={index}
-                style = {{
-                    left: ponto.clientX,
-                    top: ponto.clientY,
-                    position:'absolute',
-                    width:'20px',
-                    height:'20px',
-                    borderRadius:'50%',
-                    backgroundColor: 'blue'
-                }}></div>
-            )
+      <div id="pageGame" onClick={addDots}>
+        {pontos.map((ponto, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                left: ponto.clientX-10,
+                top: ponto.clientY-108,
+                position: "absolute",
+                width: "20px",
+                height: "20px",
+                borderRadius: "50%",
+                backgroundColor: "blue",
+              }}
+            ></div>
+          );
         })}
-        
-    </div>
+      </div>
+    </>
+  );
+};
 
-</>
-  )
-}
-
-export default CirclesOnScreen
+export default CirclesOnScreen;
